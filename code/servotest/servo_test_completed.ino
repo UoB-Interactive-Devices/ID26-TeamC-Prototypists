3b99049c22f1a05ca73f7d4136667817d4716aba
Kanghyeon5468 <142624826+Kanghyeon5468@users.noreply.github.com>
2026-04-13 17:05:31 +0100

    Update servo_test_completed.ino

--- code/servotest/servo_test_completed.ino
+++ code/servotest/servo_test_completed.ino
@@ -3,135 +3,110 @@
 
 ServoDriver servo;
 
-// channels
+// 채널 설정
 const int SHOULDER_L = 11;
-const int SHOULDER_R = 13;
+const int SHOULDER_R = 9;
 const int ELBOW      = 7;
-const int WRIST      = 4;
-const int WRIST_ROT  = 10;    // 360 degree
-const int GRIPPER    = 16;
+const int WRIST      = 5;
+const int WRIST_ROT  = 3;
+const int GRIPPER    = 1;
 
-
+// 시작 각도
 const int CENTER = 90;
-const int DELTA  = 45;
 
-// stop value for 360 degree servo
-int WRIST_ROT_STOP = 90;
+// 각도 범위
+const int MIN_ANGLE = 0;
+const int MAX_ANGLE = 180;
 
-void safeAngle(int ch, int angle) {
-  if (angle < 0) angle = 0;
-  if (angle > 180) angle = 180;
-  servo.setAngle(ch, angle);
+// 속도 조절값
+const int STEP_DELAY = 20;
+
+// 현재 각도 저장
+int shoulderLCurrent = CENTER;
+int shoulderRCurrent = CENTER;
+int elbowCurrent     = CENTER;
+int wristCurrent     = CENTER;
+int wristRotCurrent  = CENTER;
+int gripperCurrent   = CENTER;
+
+void clampAngle(int &angle) {
+  if (angle < MIN_ANGLE) angle = MIN_ANGLE;
+  if (angle > MAX_ANGLE) angle = MAX_ANGLE;
 }
 
-void stopWristRotate() {
-  servo.setAngle(WRIST_ROT, WRIST_ROT_STOP);
+void moveServoSlow(int ch, int &currentAngle, int targetAngle) {
+  clampAngle(targetAngle);
+
+  if (currentAngle < targetAngle) {
+    for (int a = currentAngle; a <= targetAngle; a++) {
+      servo.setAngle(ch, a);
+      delay(STEP_DELAY);
+    }
+  } else if (currentAngle > targetAngle) {
+    for (int a = currentAngle; a >= targetAngle; a--) {
+      servo.setAngle(ch, a);
+      delay(STEP_DELAY);
+    }
+  }
+
+  currentAngle = targetAngle;
 }
 
-void wristRotateCW() {
-  servo.setAngle(WRIST_ROT, WRIST_ROT_STOP + 20);
+void setShoulders(int leftAngle) {
+  int rightAngle = 180 - leftAngle;
+  clampAngle(leftAngle);
+  clampAngle(rightAngle);
+
+  while (shoulderLCurrent != leftAngle || shoulderRCurrent != rightAngle) {
+    if (shoulderLCurrent < leftAngle) shoulderLCurrent++;
+    else if (shoulderLCurrent > leftAngle) shoulderLCurrent--;
+
+    if (shoulderRCurrent < rightAngle) shoulderRCurrent++;
+    else if (shoulderRCurrent > rightAngle) shoulderRCurrent--;
+
+    servo.setAngle(SHOULDER_L, shoulderLCurrent);
+    servo.setAngle(SHOULDER_R, shoulderRCurrent);
+    delay(STEP_DELAY);
+  }
+
+  Serial.print("Shoulder L: ");
+  Serial.print(leftAngle);
+  Serial.print(" , Shoulder R: ");
+  Serial.println(rightAngle);
 }
 
-void wristRotateCCW() {
-  servo.setAngle(WRIST_ROT, WRIST_ROT_STOP - 20);
+void setElbow(int angle) {
+  moveServoSlow(ELBOW, elbowCurrent, angle);
+  Serial.print("Elbow: ");
+  Serial.println(angle);
 }
 
-void centerAll180Servos() {
-  safeAngle(SHOULDER_L, CENTER);
-  safeAngle(SHOULDER_R, CENTER);
-  safeAngle(ELBOW, CENTER);
-  safeAngle(WRIST, CENTER);
-  safeAngle(GRIPPER, CENTER);
+void setWrist(int angle) {
+  moveServoSlow(WRIST, wristCurrent, angle);
+  Serial.print("Wrist: ");
+  Serial.println(angle);
 }
 
-void stopAll() {
-  centerAll180Servos();
-  stopWristRotate();
+void setWristRot(int angle) {
+  moveServoSlow(WRIST_ROT, wristRotCurrent, angle);
+  Serial.print("Wrist Rotate: ");
+  Serial.println(angle);
 }
 
-void testShoulderPair() {
-  Serial.println("Testing shoulder pair");
-
-  safeAngle(SHOULDER_L, CENTER - DELTA);
-  safeAngle(SHOULDER_R, CENTER + DELTA);
-  delay(1200);
-
-  centerAll180Servos();
-  delay(800);
-
-  safeAngle(SHOULDER_L, CENTER - DELTA);
-  safeAngle(SHOULDER_R, CENTER + DELTA);
-  delay(1200);
-
-  centerAll180Servos();
-  delay(800);
+void setGripper(int angle) {
+  moveServoSlow(GRIPPER, gripperCurrent, angle);
+  Serial.print("Gripper: ");
+  Serial.println(angle);
 }
 
-void testElbow() {
-  Serial.println("Testing elbow");
-
-  safeAngle(ELBOW, CENTER - DELTA);
-  delay(1200);
-
-  safeAngle(ELBOW, CENTER);
-  delay(800);
-
-  safeAngle(ELBOW, CENTER + DELTA);
-  delay(1200);
-
-  safeAngle(ELBOW, CENTER);
-  delay(800);
-}
-
-void testWrist() {
-  Serial.println("Testing wrist");
-
-  safeAngle(WRIST, CENTER - DELTA);
-  delay(1200);
-
-  safeAngle(WRIST, CENTER);
-  delay(800);
-
-  safeAngle(WRIST, CENTER + DELTA);
-  delay(1200);
-
-  safeAngle(WRIST, CENTER);
-  delay(800);
-}
-
-void testGripper() {
-  Serial.println("Testing gripper");
-
-  safeAngle(GRIPPER, CENTER - DELTA);
-  delay(1200);
-
-  safeAngle(GRIPPER, CENTER);
-  delay(800);
-
-  safeAngle(GRIPPER, CENTER + DELTA);
-  delay(1200);
-
-  safeAngle(GRIPPER, CENTER);
-  delay(800);
-}
-
-void testWristRotate() {
-  Serial.println("Testing wrist rotate");
-
-  stopWristRotate();
-  delay(1000);
-
-  wristRotateCW();
-  delay(1000);
-
-  stopWristRotate();
-  delay(1000);
-
-  wristRotateCCW();
-  delay(1000);
-
-  stopWristRotate();
-  delay(1000);
+void centerAll() {
+  setShoulders(CENTER);
+  setElbow(CENTER);
+  setWrist(CENTER);
+  setWristRot(CENTER);
+  setGripper(CENTER);
+  Serial.println("All servos centered at 90");
+  delay(1500);
 }
 
 void setup() {
@@ -139,20 +114,49 @@ void setup() {
   Serial.begin(9600);
 
   servo.init(0x7f);
-  delay(1000);
+  delay(500);
 
-  Serial.println("Start robot arm full test");
-  stopAll();
-  delay(2000);
+  centerAll();
 }
 
 void loop() {
-  testShoulderPair();
-  testElbow();
-  testWrist();
-  testGripper();
-  testWristRotate();
+  Serial.println("Testing shoulders");
+  setShoulders(70);
+  delay(1000);
+  setShoulders(110);
+  delay(1000);
+  setShoulders(90);
+  delay(1500);
 
-  Serial.println("Test cycle complete");
-  delay(3000);
+  Serial.println("Testing elbow");
+  setElbow(60);
+  delay(1000);
+  setElbow(120);
+  delay(1000);
+  setElbow(90);
+  delay(1500);
+
+  Serial.println("Testing wrist");
+  setWrist(60);
+  delay(1000);
+  setWrist(120);
+  delay(1000);
+  setWrist(90);
+  delay(1500);
+
+  Serial.println("Testing wrist rotation");
+  setWristRot(60);
+  delay(1000);
+  setWristRot(120);
+  delay(1000);
+  setWristRot(90);
+  delay(1500);
+
+  Serial.println("Testing gripper");
+  setGripper(0);
+  delay(1000);
+  setGripper(120);
+  delay(1000);
+  setGripper(90);
+  delay(2000);
 }
